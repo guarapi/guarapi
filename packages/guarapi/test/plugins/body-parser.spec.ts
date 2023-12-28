dc06d903a486bd36969ef47d0de5cd4a6f2ee4fa
João Neto <joaopintoneto@gmail.com>
2023-12-28 19:58:38 -0300

    feat(body-parser): add application/x-www-form-urlencoded
    add maxPayloadSize for body parsers (with 2MB default)
    add .send() method
    update vscode settings.json

--- packages/guarapi/test/plugins/body-parser.spec.ts
+++ packages/guarapi/test/plugins/body-parser.spec.ts
@@ -8,7 +8,7 @@ import guarapi, {
 
 describe('Guarapi - plugins/body-parser', () => {
   const buildApp = () => {
-    const app = guarapi();
+    const app = guarapi({ maxPayloadSize: 1000 });
     const server = createServer({}, app);
 
     app.plugin(bodyParserPlugin);
@@ -86,5 +86,102 @@ describe('Guarapi - plugins/body-parser', () => {
 
     expect(bodyHandler).not.toBeCalled();
     expect(errorHandler).toBeCalled();
+  });
+
+  it('should parse x-www-form-urlencoded with malicious data', async () => {
+    const { app, server } = buildApp();
+    const body = jest.fn();
+    const maliciousPayload = "name=John&age=30&comment=<script>alert('XSS!')</script>";
+
+    app.use((req, res) => {
+      body(req.body);
+      res.end();
+    });
+
+    await request(server)
+      .post('/')
+      .set('Content-type', 'application/x-www-form-urlencoded')
+      .send(maliciousPayload)
+      .expect(200);
+
+    expect(body).toBeCalledWith({
+      age: '30',
+      comment: "<script>alert('XSS!')</script>",
+      name: 'John',
+    });
+  });
+
+  it('should handle x-www-form-urlencoded with invalid UTF-8 characters', async () => {
+    const { app, server } = buildApp();
+    const body = jest.fn();
+    const invalidPayload = 'name=John&age=30&comment=\uD800\uDC00';
+
+    app.use((req, res) => {
+      body(req.body);
+      res.end();
+    });
+
+    await request(server)
+      .post('/')
+      .set('Content-type', 'application/x-www-form-urlencoded')
+      .send(invalidPayload)
+      .expect(200);
+
+    expect(body).toBeCalledWith({ age: '30', comment: '𐀀', name: 'John' });
+  });
+
+  it('should handle malformed x-www-form-urlencoded data', async () => {
+    const { app, server } = buildApp();
+    const bodyHandler = jest.fn();
+    const errorHandler = jest.fn();
+    const malformedPayload = 'name=Guarapi%7D%20%7Bmalformed=true';
+
+    app.use((req, res) => {
+      bodyHandler(req.body);
+      res.end();
+    });
+
+    app.use<MiddlewareError>((error, req, res, _next) => {
+      errorHandler();
+      res.end();
+    });
+
+    await request(server)
+      .post('/')
+      .set('Content-type', 'application/x-www-form-urlencoded')
+      .send(malformedPayload)
+      .expect(200);
+
+    expect(bodyHandler).toBeCalledWith({ name: 'Guarapi} {malformed=true' });
+    expect(errorHandler).not.toBeCalled();
+  });
+
+  it('should handle x-www-form-urlencoded with payload overflow', async () => {
+    const { app, server } = buildApp();
+    const bodyHandler = jest.fn();
+    const errorHandler = jest.fn();
+
+    const largePayload = 'name=' + 'A'.repeat(1000000);
+
+    app.use((req, res) => {
+      bodyHandler(req.body);
+      res.end();
+    });
+
+    app.use<MiddlewareError>((error, req, res, _next) => {
+      errorHandler();
+      res.status(400).send('Payload too large');
+      res.end();
+    });
+
+    const response = await request(server)
+      .post('/')
+      .set('Content-type', 'application/x-www-form-urlencoded')
+      .send(largePayload)
+      .expect(400);
+
+    expect(bodyHandler).not.toBeCalled();
+    expect(errorHandler).toBeCalled();
+    expect(response.text).toBe('Payload too large');
   });
 });
